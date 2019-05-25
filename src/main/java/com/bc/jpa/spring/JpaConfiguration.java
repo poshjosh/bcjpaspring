47130b21ea35c86e7a9ed0a43f2736ad2c8f699e
poshjosh <posh.bc@gmail.com>
2019-05-25 01:52:05 +0100

    Initial commit

---
+++ src/main/java/com/bc/jpa/spring/JpaConfiguration.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2019 NUROX Ltd.
+ *
+ * Licensed under the NUROX Ltd Software License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.looseboxes.com/legal/licenses/software.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bc.jpa.spring;
+
+import com.bc.jpa.spring.repository.EntityRepositoryFactory;
+import com.bc.jpa.spring.repository.EntityRepositoryFactoryImpl;
+import com.bc.jpa.spring.selectors.SelectorFactory;
+import com.bc.db.meta.access.MetaDataAccess;
+import com.bc.db.meta.access.MetaDataAccessImpl;
+import com.bc.jpa.dao.JpaObjectFactory;
+import com.bc.jpa.dao.JpaObjectFactoryImpl;
+import com.bc.jpa.dao.functions.EntityManagerFactoryCreator;
+import com.bc.jpa.dao.functions.EntityManagerFactoryCreatorImpl;
+import com.bc.jpa.dao.sql.MySQLDateTimePatterns;
+import com.bc.jpa.dao.sql.SQLDateTimePatterns;
+import java.util.Collections;
+import java.util.Properties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * @author Chinomso Bassey Ikwuagwu on Apr 9, 2019 11:31:20 AM
+ */
+public abstract class JpaConfiguration {
+    
+    public abstract String getPersistenceUnitName();
+    
+    public abstract JdbcPropertiesProvider JdbcPropertiesProvider(ApplicationContext spring);
+    
+    public SelectorFactory selectorFactory(ApplicationContext spring) {
+        return (selectorName) -> Collections.EMPTY_LIST;
+    }
+    
+    public String [] getEntityPackageNames() {
+        return new String[0];
+    }
+
+    @Bean @Scope("prototype") public TypeFromNameResolver entityTypeResolver() {
+        return new TypeFromNameResolverComposite(
+                new TypeFromNameResolverUsingPersistenceXmlFile(),
+                new TypeFromNameResolverUsingPackageNames(getEntityPackageNames())
+        );
+    }
+    
+    @Bean public EntityRepositoryFactory entityServiceProvider(JpaObjectFactory jpa) {
+        return new EntityRepositoryFactoryImpl(jpa);
+    }
+    
+    @Bean @Scope("prototype") public MetaDataAccess metaDataAccess(JpaObjectFactory jpa) {
+        return new MetaDataAccessImpl(jpa.getEntityManagerFactory());
+    }
+    
+    @Bean public JpaObjectFactory jpaObjectFactory(
+            EntityManagerFactoryCreator emfCreator,
+            SQLDateTimePatterns sqlDateTimePatterns) {
+        return new JpaObjectFactoryImpl(this.getPersistenceUnitName(), emfCreator, sqlDateTimePatterns);
+    }
+    
+    @Bean @Scope("prototype") public SQLDateTimePatterns sqlDateTimePatterns(JdbcPropertiesProvider jdbcPropertiesProvider) {
+        // @todo parse properties and determine if driver is: myql, postgresql etc
+        // and then return the corresponding datetimepatterns object
+        return new MySQLDateTimePatterns();
+    }
+    
+    @Bean @Scope("prototype") public EntityManagerFactoryCreator entityManagerFactoryCreator(
+            JdbcPropertiesProvider jdbcPropertiesProvider) {
+        return new EntityManagerFactoryCreatorImpl(){
+            @Override
+            protected Properties getProperties(String persistenceUnit) {
+                return jdbcPropertiesProvider.apply(persistenceUnit); 
+            }
+        };
+    }
+}
---
+++ src/main/java/com/bc/jpa/spring/repository/EntityRepositoryFactoryImpl.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 NUROX Ltd.
+ *
+ * Licensed under the NUROX Ltd Software License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.looseboxes.com/legal/licenses/software.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bc.jpa.spring.repository;
+
+import com.bc.jpa.dao.JpaObjectFactory;
+import com.bc.jpa.dao.functions.GetTableNameFromAnnotation;
+import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Chinomso Bassey Ikwuagwu on Apr 6, 2019 2:05:27 PM
+ */
+public class EntityRepositoryFactoryImpl implements EntityRepositoryFactory{
+
+    private static final Logger LOG = LoggerFactory.getLogger(EntityRepositoryFactoryImpl.class);
+    
+    private final JpaObjectFactory jpa;
+    
+    public EntityRepositoryFactoryImpl(JpaObjectFactory jpa) {
+        this.jpa = Objects.requireNonNull(jpa);
+    }
+    
+    @Override
+    public boolean isSupported(Class entityType) {
+        try{
+            new GetTableNameFromAnnotation().apply(entityType);
+            return true;
+        }catch(RuntimeException ignored) {
+            LOG.trace("Failed to resolve table name from type: {}", entityType);
+            return false;
+        }
+    }
+    
+    @Override
+    public EntityRepository forEntity(Class entityType) {
+        return new EntityRepositoryImpl(jpa, entityType);
+    }
+
+    public JpaObjectFactory getJpa() {
+        return jpa;
+    }
+}
