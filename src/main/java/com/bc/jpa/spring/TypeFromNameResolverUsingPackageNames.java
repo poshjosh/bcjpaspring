260dd8cf8b348c1754597fecb68637efe234d3b2
poshjosh <posh.bc@gmail.com>
2020-05-09 17:01:11 +0100

    Improve logic for resolution of class from specified name
    Rather than just appending the specified name (as class name) to the 
    package name, search through the package for classes matching the 
    specified name.

--- src/main/java/com/bc/jpa/spring/TypeFromNameResolverUsingPackageNames.java
+++ src/main/java/com/bc/jpa/spring/TypeFromNameResolverUsingPackageNames.java
@@ -13,12 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.bc.jpa.spring;
 
+import com.bc.reflection.function.FindClassesInPackage;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
+import javax.persistence.Table;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,35 +31,51 @@ public class TypeFromNameResolverUsingPackageNames extends AbstractEntityTypeRes
 
     private static final Logger LOG = LoggerFactory.getLogger(TypeFromNameResolverUsingPackageNames.class);
     
+    public static class ClassNotFoundForNameException extends RuntimeException{
+        public ClassNotFoundForNameException() { }
+        public ClassNotFoundForNameException(String string) {
+            super(string);
+        }
+    }
+
     private final List<String> packageNames;
+    
+    private final Function<String, List<Class>> findClassesInPackage;
 
     public TypeFromNameResolverUsingPackageNames(String... packageNames) {
         this(Arrays.asList(packageNames));
     }
     
     public TypeFromNameResolverUsingPackageNames(List<String> packageNames) {
-        this.packageNames = Objects.requireNonNull(packageNames);
+        this(packageNames, new FindClassesInPackage());
     }
     
+    public TypeFromNameResolverUsingPackageNames(List<String> packageNames, 
+            Function<String, List<Class>> findClassesInPackage) {
+        this.packageNames = Objects.requireNonNull(packageNames);
+        this.findClassesInPackage = Objects.requireNonNull(findClassesInPackage);
+    }
+
     @Override
     public Class getType(String entityName) {
 
         final Class type = getType(entityName, (Class)null);
         
         if(type == null) {
-            throw new RuntimeException("Failed to find class named: " + entityName + 
+            throw new ClassNotFoundForNameException(
+                    "Failed to find class for name: " + entityName + 
                     ", in packages: " + packageNames);
         }
         
         return type;
     }
-    
+
     @Override
     public Class getType(String entityName, Class resultIfNone) {
         
         for(String packageName : packageNames) {
-        
-            final Class type = getEntityType(packageName, entityName);
+
+            final Class type = this.getType(entityName, packageName, null);
             
             if(type != null) {
                 
@@ -67,19 +85,18 @@ public class TypeFromNameResolverUsingPackageNames extends AbstractEntityTypeRes
         
         return resultIfNone;
     }
-
-    public Class getEntityType(String packageName, String entityName) {
-        
-        final String className = packageName + '.' + entityName;
-        
-        try{
-            return Class.forName(className);
-        }catch(Exception ignored) {
-            
-            LOG.debug("Failed to load class: {}, which was resolved to package name: {}",
-                    className, packageName);
-            
-            return null;
+    
+    public Class getType(String tableName, String packageName, Class resultIfNone) {
+        final List<Class> list = this.findClassesInPackage.apply(packageName);
+        for(Class cls : list) {
+            if(cls.getName().equalsIgnoreCase(tableName)) {
+                return cls;
+            }
+            final Table en = (Table)cls.getAnnotation(Table.class);
+            if(Objects.equals(en == null ? null : en.name(), tableName)) {
+                return cls;
+            }
         }
+        return resultIfNone;
     }
 }
